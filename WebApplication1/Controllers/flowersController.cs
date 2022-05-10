5fbdee5a1f09da322c7c448eee98e4a5fa8ac824
Frankie White <FrankieWhite@exe-coll.ac.uk>
2022-05-10 15:32:18 +0100

    Add project files.

---
+++ WebApplication1/Controllers/flowersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Views.flowers;
+
+namespace WebApplication1.Controllers
+{
+    public class flowersController : Controller
+    {
+        // GET: flowers
+        public ActionResult Index()
+        {
+            List<flowersModel> flowers = new List<flowersModel>();
+
+            flowers.Add(new flowersModel(1, "Rose", "june", "red", 30));
+            flowers.Add(new flowersModel(2, "Lilija", "may", "yelow", 40));
+            flowers.Add(new flowersModel(3, "Erimurus", "august", "pink", 80));
+            flowers.Add(new flowersModel(4, "Esholcija", "april", "orange", 20));
+            
+            return View("index", flowers);
+        }
+
+    }
+}
---
+++ WebApplication1/Views/flowers/flowersModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Views.flowers
+{
+    public class flowersModel
+    {
+        public int Id { get; set; }
+
+        public String FName { get; set; }
+
+        public String Flowering { get; set; }
+
+        public String Colours { get; set; }
+
+        public int Size { get; set; }
+
+        public flowersModel(int id, string fName, string flowering, string colours, int size)
+        {
+            Id = id;
+            FName = fName;
+            Flowering = flowering;
+            Colours = colours;
+            Size = size;
+        }
+
+    }
+
+}
